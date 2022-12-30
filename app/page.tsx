f58e71117072ef15057eb4ecec5f88b60d522709
Juan Pujol <joangalilea@gmail.com>
2022-12-30 10:10:02 +0100

    Add project base

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Button from "../components/Atoms/Button/Button";
+import Layout from "./layout";
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <Button>Hello</Button>
+    </Layout>
+  )
+}
---
+++ components/Atoms/Button/Button.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+
+import styled from "styled-components";
+
+
+const Button = styled.button`
+    background-color: #aaa;
+    border: 1px solid #000;
+`;
+
+export default Button;
