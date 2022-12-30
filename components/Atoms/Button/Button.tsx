ff025150821d573ae136e00b6305d4f1b584b1b4
Juan Pujol <joangalilea@gmail.com>
2022-12-30 11:35:30 +0100

    Add basic breakpoints

--- components/Atoms/Button/Button.tsx
+++ components/Atoms/Button/Button.tsx
@@ -2,11 +2,16 @@
 
 
 import styled from "styled-components";
+import { breakpoints } from "../../../styles/breakpoints";
 
 
 const Button = styled.button`
-    background-color: ${props => props.theme.colors.primary};
-    border: 1px solid #000;
+  background-color: ${props => props.theme.colors.primary};
+  border: 1px solid #000;
+
+  @media ${breakpoints.mobile} {
+    background-color: red;
+  }
 `;
 
 export default Button;
---
+++ styles/breakpoints.ts
@@ -0,0 +1,6 @@
+export const breakpoints = {
+  desktopLarge: `(max-width: 1440px)`,
+  desktop: `(max-width: 1024px)`,
+  tablet: `(max-width: 768px)`,
+  mobile: `(max-width: 375px)`,
+};
